8558ddca982461d885e7f9cd1c371d3a81dd5235
Peter Adelmann <peter.adelmann@trilobit.de>
2024-03-20 18:12:08 +0100

    php8 and contao 5

--- src/Resources/contao/languages/en/tl_form_field.php
+++ src/Resources/contao/languages/en/tl_form_field.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * @copyright  trilobit GmbH
  * @author     trilobit GmbH <https://github.com/trilobit-gmbh>
  * @license    LGPL-3.0-or-later
- * @link       http://github.com/trilobit-gmbh/contao-multiformgroup-bundle
  */
 
 $GLOBALS['TL_LANG']['tl_form_field']['multi_form_group_legend'] = 'Multi Form Group Settings';
